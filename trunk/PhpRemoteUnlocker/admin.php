aa3deecd34955902ffceab7145c4cea78cf45e05
luigiva@gmail.com <luigiva@gmail.com@8a5ecdb2-f60b-83ff-167c-6c6b67a5cd0b>
2011-09-28 09:39:16 +0000

    [PHP Backend] Initial import

---
+++ trunk/PhpRemoteUnlocker/admin.php
@@ -0,0 +1,79 @@
+<?php
+
+session_set_cookie_params(3600);
+session_start();
+
+include_once "lib/config.php";
+include_once "lib/database.php";
+
+$pwd = $_POST['pwd'];
+$newSerial = $_POST['new_serial'];
+$delete = $_GET['delete'];
+$edit = $_GET['edit'];
+
+if (isset($pwd)) {
+	if ($pwd == Config::RU_PASSWORD) $_SESSION['authorized'] = true;
+	else $wrongPassword = "<p>Wrong password</p>";
+}
+
+$db = new Database();
+
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 //EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>RemoteUnlocker Serial Manager</title>
+</head>
+
+<body>
+
+<?php if (!isset($_SESSION['authorized']) || $_SESSION['authorized'] == false): ?>
+
+<?php if (isset($wrongPassword)) echo $wrongPassword; ?>
+	<form name="Login" action="<?php echo $_SERVER['PHP_SELF']; ?>"
+		method="POST">
+		Password: <input type="password" name="pwd" /> <input type="submit"
+			value="Login" />
+	</form>
+		
+	<?php else: ?>
+	
+	<?php
+	
+	if (isset($newSerial)) {
+		if ($db->insertSerial($newSerial)) echo "<p>New serial correctly added</p>";
+		else echo "<p>Serial not added because of DB fault</p>";
+	}
+	
+	if (isset($delete)) {
+		if ($db->deleteSerial($delete)) echo "Serial correctly deleted";
+		else echo "Error deleting serial";
+	}
+	
+	?>
+	
+	<form name="newserial" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+	New serial: <input type="text" name="new_serial"/>
+	<input type="submit" value="Add" />
+	</form>
+	
+	<table border="1">
+		<tr>
+			<th>ID</th>
+			<th>Serial</th>
+			<th>Device ID</th>
+			<th>Options</th>
+		</tr>
+		
+			<?php $db->createSerialTable(); ?>
+			
+	</table>
+	
+	<?php 
+	endif;
+	unset($db); 
+	?>
+
+	</body>
+</html>
