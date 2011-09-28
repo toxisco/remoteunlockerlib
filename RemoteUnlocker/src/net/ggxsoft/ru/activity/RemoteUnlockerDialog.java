acd73d9704a46331c358a7cc89319481d70e6de9
luigiva <luigiva@gmail.com>
2011-09-28 09:36:05 +0000

    [Android Library] Initial import

---
+++ RemoteUnlocker/src/net/ggxsoft/ru/activity/RemoteUnlockerDialog.java
@@ -0,0 +1,147 @@
+/*
+ * RemoteUnlockerDialog (RemoteUnlocker Lib)
+ * Copyright (C) 2011 gGxSoft.net
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.ggxsoft.ru.activity;
+
+import net.ggxsoft.remoteunlocker.R;
+import net.ggxsoft.ru.client.Connecter;
+import net.ggxsoft.ru.client.ValidateAsync;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class RemoteUnlockerDialog extends Dialog {
+	
+	protected LinearLayout main, buttonsLayout;
+	protected Connecter link;
+	protected Button ok,
+					cancel;
+	protected EditText serial;
+	protected Context context;
+	protected TelephonyManager tm;
+	protected TextView message, powered;
+	
+	/** 
+	 * Crea un oggetto di tipo Dialog. Se è presente un codice di sblocco valido vengono 
+	 * disabilitati sia il campo di inserimento del seriale che il pulsante "Ok".
+	 * 
+	 * @param context	il Context di riferimento
+	 * @param serverAddress	l'indirizzo dove è localizzato lo script rucheck.php
+	 * */
+	public RemoteUnlockerDialog(Context context, String serverAddress) {
+		super(context);
+		this.context = context;
+		tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		link = new Connecter(serverAddress);
+		initViews();
+		if (RemoteUnlocker.getUnlockResultCode(context) == RemoteUnlocker.SERIAL_OK) {
+			serial.setText(R.string.app_already_unlocked);
+			serial.setEnabled(false);
+			serial.setInputType(InputType.TYPE_NULL);
+			ok.setEnabled(false);
+		}
+	}
+
+	/** 
+	 * Crea un AsyncTask per la validazione del codice
+	 * */
+	protected void validateProcedure() {
+		if (serial.getText().toString().equals("")) return;
+		new ValidateAsync(context, link).execute(serial.getText().toString(), tm.getDeviceId());
+		this.dismiss();
+	}
+
+	/** 
+	 * Metodo di setup degli elementi grafici del Dialog
+	 * */
+	protected void initViews() {
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1);
+		main = new LinearLayout(context);
+		main.setOrientation(LinearLayout.VERTICAL);
+		buttonsLayout = new LinearLayout(context);
+		ok = new Button(context);
+		ok.setLayoutParams(params);
+		cancel = new Button(context);
+		cancel.setLayoutParams(params);
+		serial = new EditText(context);
+		message = new TextView(context);
+		powered = new TextView(context);
+		
+		main.setPadding(5, 0, 5, 10);
+		
+		buttonsLayout.setOrientation(LinearLayout.HORIZONTAL);
+		buttonsLayout.addView(ok);
+		buttonsLayout.addView(cancel);
+		buttonsLayout.setGravity(Gravity.CENTER);
+		buttonsLayout.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		buttonsLayout.setWeightSum(2);
+		
+		ok.setText(R.string.ok);
+		cancel.setText(R.string.cancel);
+		
+		ok.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				validateProcedure();
+			}
+		});
+		
+		cancel.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				RemoteUnlockerDialog.this.dismiss();
+			}
+		});
+		
+		serial.setGravity(Gravity.CENTER);
+		serial.setWidth(240);
+		message.setGravity(Gravity.CENTER);
+		message.setText(R.string.insert_unlock);
+		message.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		powered.setText("Powered by RemoteUnlocker");
+		powered.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+		powered.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://ggxsoft.net")));
+			}
+		});
+		
+		main.addView(message);
+		main.addView(serial);
+		main.addView(buttonsLayout);
+		main.addView(powered);
+		
+		this.setContentView(main);
+		this.setTitle(R.string.unlock);
+	}
+		
+}
---
+++ RemoteUnlocker/src/net/ggxsoft/ru/client/ValidateAsync.java
@@ -0,0 +1,107 @@
+/*
+ * ValidateAsync (RemoteUnlocker Lib)
+ * Copyright (C) 2011 gGxSoft.net
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.ggxsoft.ru.client;
+
+import java.io.IOException;
+
+import net.ggxsoft.remoteunlocker.R;
+import net.ggxsoft.ru.activity.RemoteUnlocker;
+
+import org.apache.http.client.ClientProtocolException;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+
+public class ValidateAsync extends AsyncTask<String, Void, Integer> {
+
+	protected Context context;
+	protected ProgressDialog dialog;
+	protected Connecter link;
+	
+	/** 
+	 * Crea un oggetto di tipo AsyncTask
+	 * 
+	 * @param context	il Context di riferimento
+	 * @param link	oggetto {@link Connecter} per la gestione della connessione
+	 * */
+	public ValidateAsync(Context context, Connecter link) {
+		this.context = context;
+		this.link = link;
+	}
+	
+	/** 
+	 * Viene impostata e visualizzata una ProgressDialog
+	 * */
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		dialog = new ProgressDialog(context);
+		dialog.setTitle(R.string.progressdialog_title);
+		dialog.setMessage(context.getString(R.string.progressdialog_message));
+		dialog.show();
+	}
+	
+	/** 
+	 * Elaborazione della connessione e controllo del codice online
+	 * 
+	 * @param arg0	param-arg composto da seriale (1° param) e imei (2° param)
+	 * 
+	 * @return	codice di stato del seriale (codici statici in {@link RemoteUnlocker})
+	 * */
+	@Override
+	protected Integer doInBackground(String... arg0) {
+		String serial = arg0[0], imei = arg0[1];
+		int response = 0;
+		try {
+			response = link.getSerialStatus(serial,imei);
+		} catch (ClientProtocolException e) {
+			return RemoteUnlocker.SERVER_NO_CONNECTION;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
+	
+	/** 
+	 * Il risultato del controllo online viene memorizzato nelle SharedPreferences:
+	 * il nome delle preferenze e quello dell'attributo è memorizzato staticamente
+	 * in {@link RemoteUnlocker}.
+	 * 
+	 * La ProgressDialog viene chiusa
+	 * 
+	 * @param result	il codice di stato ricevuto dalla validazione
+	 * */
+	@Override
+	protected void onPostExecute(Integer result) {
+		super.onPostExecute(result);
+		
+		SharedPreferences prefs = context.getSharedPreferences(RemoteUnlocker.PREFERENCES_NAME, Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putInt(RemoteUnlocker.PREFERENCES_ATTRIBUTE, result);
+		editor.commit();
+		
+		dialog.dismiss();
+		
+		((Unlockable)context).onUnlockResponse(result);
+			
+	}
+
+}
